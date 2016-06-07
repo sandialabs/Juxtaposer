a5c467e80b336a079cd346ea377ce903832914b5
Corey Hudson <cmhudso@sandia.gov>
2016-06-07 15:59:15 -0700

    Fixed attct.pl problems

--- attct.pl
+++ attct.pl
@@ -1,24 +1,25 @@
-#! /usr/bin/perl -w
+#! /usr/bin/perl
 use strict;
+use warnings;
 # Not paying attention to partner reads
 
-my (%queries, %reads, %regexs, @islands, $island, $att, %hits);
-open IN, "/data1/users/kpwilli/mitomycin/atts.fa" or die "No atts.fa file\n";
+die "Usage: $0 atts-fastA-file read-fastQ-file(s)\n" unless @ARGV > 1;
+my (%queries, %reads, %regexs, $island, $att, %hits);
+open IN, $ARGV[0] or die "No atts.fa file\n";
 while (<IN>) {
  chomp;
- if (/^>(\S+)\.([LRBP])$/) {$island = $1; $att = $2; next}
+ if (/^>/) {
+  die "Att.fa headers require an internal period, eg >EcoX.L\n" unless /^>(\S+)\.(\S+)$/;
+  ($island, $att) = ($1, $2);
+  next
+ }
  s/-//g;
- #print "'$_'\n";
  $queries{$island}{$att}{seq} .= uc $_;
- #print "$island $att '", uc($_), "'\n";
 }
 close IN;
-#die scalar(keys %queries), " att sets\n";
 
 for my $island (sort keys %queries) {
- unless (keys %{$queries{$island}} == 4) {warn "Not all 4 atts given for island $island; skip\n"; next}
- push @islands, $island;
- for my $att (qw/L R B P/) {
+ for my $att (sort keys %{$queries{$island}}) {
   $regexs{"$island.$att.fwd"} = qr/$queries{$island}{$att}{seq}/;
   my $revcomp = Revcomp($queries{$island}{$att}{seq});
   $regexs{"$island.$att.rev"} = qr/$revcomp/;
@@ -26,34 +27,31 @@ for my $island (sort keys %queries) {
 }
 
 my $seq;
-open IN, "$ARGV[0]" or die "No fastq file $ARGV[0]\n";
-while (<IN>) {
- my $seq = <IN>;
- <IN>; <IN>;
- chomp $seq;
- #print "$seq\n";
- for my $att (keys %regexs) {
-  #print "$att $regexs{$att}\n";
-  next unless $seq =~ $regexs{$att};
-  #print "hi\n";
-  $att =~ /^(.*)\.(rev|fwd)$/;
-  #print "$1 $2\n";
-  $seq = Revcomp($seq) if $2 eq 'rev';
-  push @{$hits{$1}}, $seq;
-  #print "$hits{$1}[0]\n";
+for (1..$#ARGV) {
+ open IN, "$ARGV[$_]" or die "No fastq file $ARGV[$_]\n";
+ while (<IN>) {
+  my $seq = <IN>;
+  <IN>; <IN>;
+  chomp $seq;
+  for my $att (keys %regexs) {
+   next unless $seq =~ $regexs{$att};
+   $att =~ /^(.*)\.(rev|fwd)$/;
+   $seq = Revcomp($seq) if $2 eq 'rev';
+   push @{$hits{$1}}, $seq;
+  }
  }
- #print "yes\n" if $seq =~ /CAAGTAATCTTCGGCATAA/;
- #exit;
+ close IN;
 }
-close IN;
 
-open OUT, ">$ARGV[0].atts";
-for my $island (@islands) {
- my %ct;
- for my $att (qw/P B L R/) {if ($hits{"$island.$att"}) {$ct{$att} = scalar(@{$hits{"$island.$att"}})} else {$ct{$att} = 0}}
- print OUT "# $island PBLR\t$ct{P}\t$ct{B}\t$ct{L}\t$ct{R}\n";
- for my	$att (qw/P B L R/) {if ($hits{"$island.$att"}) {print OUT join("\n", "# $island $att", @{$hits{"$island.$att"}}), "\n\n"}}
+for my $island (sort keys %queries) {
+ my (%ct, @atts);
+ for my $att (sort keys %{$queries{$island}}) {
+  push @atts, $att;
+  if ($hits{"$island.$att"}) {$ct{$att} = scalar(@{$hits{"$island.$att"}})}
+  else {$ct{$att} = 0}
+ }
+ print "# $island ", join(',', @atts); for (@atts) {print "\t$ct{$_}"} print "\n";
+ for my	$att (@atts) {if ($hits{"$island.$att"}) {print join("\n", "# $island $att", @{$hits{"$island.$att"}}), "\n\n"}}
 }
-close OUT;
 
 sub Revcomp {my $ret = reverse $_[0]; $ret =~ tr/ACGT/TGCA/; return $ret}
